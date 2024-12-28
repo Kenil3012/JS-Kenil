11b38db22373a4f878309f72143d26b3731e83bf
Kenil3012 <154608736+Kenil3012@users.noreply.github.com>
2024-12-28 18:06:49 +0000

    Learn Variables

---
+++ JS_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1
+let accountEmail = "kenil@gmail.com"
+var accountPass = "12345"
+accountCity = "Surat"
+accountstate; 
+
+ 
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
